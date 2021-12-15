ce1e587239267898249bdcbb379bedf2bb9ad482
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-15 11:52:14 +0100

    first commit

---
+++ src/test/java/me/mathieusoysal/codingame-stats/AppTest.java
@@ -0,0 +1,20 @@
+package me.mathieusoysal.codingame-stats;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
