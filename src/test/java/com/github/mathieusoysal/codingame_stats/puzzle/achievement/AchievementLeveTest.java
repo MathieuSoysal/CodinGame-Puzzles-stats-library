5a075652cf45d2decb88333f15140e20e69fe775
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-17 23:16:09 +0100

    ✅ Add valided test : AchievementLevel

--- src/test/java/com/github/mathieusoysal/codingame_stats/puzzle/achievement/AchievementLeveTest.java
+++ src/test/java/com/github/mathieusoysal/codingame_stats/puzzle/achievement/AchievementLeveTest.java
@@ -2,10 +2,13 @@ package com.github.mathieusoysal.codingame_stats.puzzle.achievement;
 
 import java.util.Comparator;
 
+import com.google.gson.Gson;
+
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
+import org.junit.jupiter.api.Nested;
 
 public class AchievementLeveTest {
 
@@ -23,6 +26,36 @@ public class AchievementLeveTest {
         assertThat(AchievementLevel.BRONZE, lessThan(AchievementLevel.SILVER));
         assertThat(AchievementLevel.BRONZE, lessThan(AchievementLevel.GOLD));
         assertThat(AchievementLevel.SILVER, lessThan(AchievementLevel.GOLD));
+    }
+
+    @Nested
+    class TestConversion {
+        private class TestClassConversion {
+
+            public AchievementLevel level;
+
+        }
+
+        @Test
+        public void testConversionWithBronze() {
+            String json = "{\"level\":\"BRONZE\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(AchievementLevel.BRONZE));
+        }
+
+        @Test
+        public void testConversionWithSilver() {
+            String json = "{\"level\":\"SILVER\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(AchievementLevel.SILVER));
+        }
+
+        @Test
+        public void testConversionWithGold() {
+            String json = "{\"level\":\"GOLD\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(AchievementLevel.GOLD));
+        }
     }
 
 }
