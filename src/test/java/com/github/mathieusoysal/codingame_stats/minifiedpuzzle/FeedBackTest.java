17ed2f6a8b5709b51e5baf051fc6cc96e8281003
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-18 00:33:31 +0100

    🧪 Add failing test : FeedBack

--- src/test/java/com/github/mathieusoysal/codingame_stats/minifiedpuzzle/FeedBackTest.java
+++ src/test/java/com/github/mathieusoysal/codingame_stats/minifiedpuzzle/FeedBackTest.java
@@ -2,7 +2,11 @@ package com.github.mathieusoysal.codingame_stats.minifiedpuzzle;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.google.gson.Gson;
 
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +25,19 @@ public class FeedBackTest {
         assertEquals(3, feedBack.getNbStarsFor(2));
         assertEquals(1, feedBack.getNbStarsFor(3));
         assertEquals(5, feedBack.getNbStarsFor(4));
+    }
+
+    @Test
+    void testConversionFromJsonFeedBack() {
+        String json = "{\"feedbackId\":1649,\"feedbacks\":[4,0,1,16,31]}";
+        FeedBack feedBack = new Gson().fromJson(json, FeedBack.class);
+        assertEquals(1649, feedBack.feedBackId());
+    }
+
+    @Test
+    void testConversionFromJsonFeedBackWithNullAndEmpty() {
+        String json = "{\"feedbackId\":1649,\"feedbacks\":[4,0,1,16,31]}";
+        FeedBack feedBack = new Gson().fromJson(json, FeedBack.class);
+        assertArrayEquals(new int[] { 4, 0, 1, 16, 31 }, feedBack.feedBacks());
     }
 }
