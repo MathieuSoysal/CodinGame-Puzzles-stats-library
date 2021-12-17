2ac1f483e44294a18ff9c7deace72144cbe2caff
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-17 23:21:50 +0100

    ✅ Add valided test : PuzzleLevelTest

--- src/test/java/com/github/mathieusoysal/codingame_stats/minifiedpuzzle/PuzzleLevelTest.java
+++ src/test/java/com/github/mathieusoysal/codingame_stats/minifiedpuzzle/PuzzleLevelTest.java
@@ -3,6 +3,10 @@ package com.github.mathieusoysal.codingame_stats.minifiedpuzzle;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.lessThan;
+
+import com.google.gson.Gson;
+
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 public class PuzzleLevelTest {
@@ -25,6 +29,73 @@ public class PuzzleLevelTest {
         assertThat(PuzzleLevel.MULTI.isSpecialLevel(), is(true));
         assertThat(PuzzleLevel.CODEGOLF.isSpecialLevel(), is(true));
         assertThat(PuzzleLevel.OPTIM.isSpecialLevel(), is(true));
+    }
+
+
+    @Nested
+    class TestConversionFromGson {
+        private class TestClassConversion {
+
+            public PuzzleLevel level;
+
+        }
+
+        @Test
+        public void testConversionWithTutorial() {
+            String json = "{\"level\":\"tutorial\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(PuzzleLevel.TUTORIAL));
+        }
+
+        @Test
+        public void testConversionWithEasy() {
+            String json = "{\"level\":\"easy\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(PuzzleLevel.EASY));
+        }
+
+        @Test
+        public void testConversionWithMedium() {
+            String json = "{\"level\":\"medium\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(PuzzleLevel.MEDIUM));
+        }
+
+        @Test
+        public void testConversionWithHard() {
+            String json = "{\"level\":\"hard\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(PuzzleLevel.HARD));
+        }
+
+        @Test
+        public void testConversionWithExpert() {
+            String json = "{\"level\":\"expert\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(PuzzleLevel.EXPERT));
+        }
+
+        @Test
+        public void testConversionWithMulti() {
+            String json = "{\"level\":\"multi\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(PuzzleLevel.MULTI));
+        }
+
+        @Test
+        public void testConversionWithCodegolf() {
+            String json = "{\"level\":\"codegolf\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(PuzzleLevel.CODEGOLF));
+        }
+
+        @Test
+        public void testConversionWithOptim() {
+            String json = "{\"level\":\"optim\"}";
+            TestClassConversion testLevel = new Gson().fromJson(json, TestClassConversion.class);
+            assertThat(testLevel.level, is(PuzzleLevel.OPTIM));
+        }
+
     }
 
 }
