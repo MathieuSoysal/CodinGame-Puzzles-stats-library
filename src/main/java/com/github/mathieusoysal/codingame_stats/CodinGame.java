37ce6173e1d67aaab12affc8ffbd37a3facfcdc3
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-17 18:34:28 +0100

    🧪 Add failling test : CodinGame

--- src/main/java/com/github/mathieusoysal/codingame_stats/CodinGame.java
+++ src/main/java/com/github/mathieusoysal/codingame_stats/CodinGame.java
@@ -1,8 +1,13 @@
 package com.github.mathieusoysal.codingame_stats;
 
+import com.github.mathieusoysal.codingame_stats.minifiedpuzzle.MinifiedPuzzle;
+
+import java.util.Arrays;
 import java.util.List;
 
 import com.github.mathieusoysal.codingame_stats.puzzle.Puzzle;
+import kong.unirest.HttpResponse;
+import kong.unirest.Unirest;
 
 public class CodinGame implements InterfaceCodinGame {
 
@@ -13,8 +18,12 @@ public class CodinGame implements InterfaceCodinGame {
 
     @Override
     public List<Puzzle> getPuzzles() {
-        // TODO Auto-generated method stub
         return null;
     }
+
+    List<MinifiedPuzzle> getAllMinifiedPuzzles() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
 
 }
--- src/test/java/com/github/mathieusoysal/codingame_stats/CodinGameTest.java
+++ src/test/java/com/github/mathieusoysal/codingame_stats/CodinGameTest.java
@@ -1,18 +1,36 @@
 package com.github.mathieusoysal.codingame_stats;
 
+import com.github.mathieusoysal.codingame_stats.minifiedpuzzle.MinifiedPuzzle;
 import com.github.mathieusoysal.codingame_stats.puzzle.Puzzle;
 import java.util.List;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class CodinGameTest {
 
+    @Disabled("TODO: implement")
     @Test
     public void testGetPuzzles() {
         CodinGame codinGame = new CodinGame();
         List<Puzzle> puzzles = codinGame.getPuzzles();
         assertThat(puzzles.size(), greaterThan(0));
+    }
+
+    @Test
+    void testAllMinifiedPuzzles() {
+        CodinGame codinGame = new CodinGame();
+        List<MinifiedPuzzle> minifiedPuzzles = codinGame.getAllMinifiedPuzzles();
+        assertThat(minifiedPuzzles.size(), greaterThan(0));
+    }
+
+    @Test
+    void testAllMinifiedPuzzlesNotNull() {
+        CodinGame codinGame = new CodinGame();
+        List<MinifiedPuzzle> minifiedPuzzles = codinGame.getAllMinifiedPuzzles();
+        assertThat(minifiedPuzzles, everyItem(notNullValue()));
     }
 
 }
