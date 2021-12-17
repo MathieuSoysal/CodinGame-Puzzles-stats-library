93f4f2abedce6d2f5cbc37840c8ace3fd975eb18
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-17 21:39:14 +0100

    🔨 Validation test : FeedBack

--- src/main/java/com/github/mathieusoysal/codingame_stats/minifiedpuzzle/FeedBack.java
+++ src/main/java/com/github/mathieusoysal/codingame_stats/minifiedpuzzle/FeedBack.java
@@ -1,34 +1,17 @@
 package com.github.mathieusoysal.codingame_stats.minifiedpuzzle;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
-/**
- * 
- */
-public class FeedBack {
-
-    /**
-     * Default constructor
-     */
-    public FeedBack() {
-    }
-
-    /**
-     * 
-     */
-    private int feedBackId;
-
-    /**
-     * 
-     */
-    private int[] feedBacks;
+public record FeedBack(int feedBackId,
+        int[] feedBacks) {
 
     /**
      * @return
      */
     public double getMoy() {
-        // TODO implement here
-        return 0.0d;
+        return IntStream.of(feedBacks).sum() / (double) feedBacks.length;
     }
 
     /**
@@ -36,8 +19,30 @@ public class FeedBack {
      * @return
      */
     public int getNbStarsFor(int numStar) {
-        // TODO implement here
-        return 0;
+        return feedBacks[numStar - 1];
+    }
+
+    @Override
+    public String toString() {
+        return "FeedBack{" +
+                "feedBackId=" + feedBackId +
+                ", feedBacks=" + Arrays.toString(feedBacks) +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        FeedBack feedBack = (FeedBack) o;
+        return feedBackId == feedBack.feedBackId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(feedBackId);
     }
 
 }
