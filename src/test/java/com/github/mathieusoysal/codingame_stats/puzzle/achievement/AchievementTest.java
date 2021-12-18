15037cb7f770389046dc4ee1b09b4179f994d406
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-18 09:51:26 +0100

    ♻️ Refactor test : Achievement

--- src/test/java/com/github/mathieusoysal/codingame_stats/puzzle/achievement/AchievementTest.java
+++ src/test/java/com/github/mathieusoysal/codingame_stats/puzzle/achievement/AchievementTest.java
@@ -3,11 +3,12 @@ package com.github.mathieusoysal.codingame_stats.puzzle.achievement;
 import com.google.gson.Gson;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class AchievementTest {
 
-    String json = """
+    final static String JSON_ACHIEVEMENT = """
                 {
                     \"id\": \"PZ_50P_P50\",
                     \"puzzleId\": 50,
@@ -23,69 +24,65 @@ public class AchievementTest {
                 }
             """;
 
+    Achievement achievement;
+
+    @BeforeEach
+    public void setUp() {
+        achievement = new Gson().fromJson(JSON_ACHIEVEMENT, Achievement.class);
+    }
+
     @Test
     void testGetId() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals("PZ_50P_P50", achievement.getId());
     }
 
     @Test
     void testGetPuzzleId() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals(50, achievement.getPuzzleId());
     }
 
     @Test
     void testGetTitle() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals("50% War", achievement.getTitle());
     }
 
     @Test
     void testGetDescription() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals("Reach a 50% score on War.", achievement.getDescription());
     }
 
     @Test
     void testGetPoints() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals(50, achievement.getPoints());
     }
 
     @Test
     void testGetProgress() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals(0, achievement.getProgress());
     }
 
     @Test
     void testGetProgressMax() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals(1, achievement.getProgressMax());
     }
 
     @Test
     void testGetCompletionTime() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals(0, achievement.getCompletionTime());
     }
 
     @Test
     void testGetImageBinaryId() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals(1924507038929L, achievement.getImageBinaryId());
     }
 
     @Test
     void testGetLevel() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals(AchievementLevel.SILVER, achievement.getLevel());
     }
 
     @Test
     void testGetWeight() {
-        Achievement achievement = new Gson().fromJson(json, Achievement.class);
         assertEquals(7000.0, achievement.getWeight());
     }
 
