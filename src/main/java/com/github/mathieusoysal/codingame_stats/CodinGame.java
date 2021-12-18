03936876d880bb717139dec5338c7a597b1fde5d
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-18 12:27:30 +0100

    ⚰️ Remove obsolete code

--- src/main/java/com/github/mathieusoysal/codingame_stats/CodinGame.java
+++ src/main/java/com/github/mathieusoysal/codingame_stats/CodinGame.java
@@ -7,18 +7,12 @@ import com.github.mathieusoysal.codingame_stats.util.CodinGameApi;
 import java.util.List;
 import java.util.function.Function;
 
-public class CodinGame implements InterfaceCodinGame {
+public class CodinGame {
     private static final Function<Puzzle, Puzzle> ifPuzzleHasAchievementGetPuzzleWithAchievementInfo = puzzle -> puzzle
             .getAchiviementCount() > 0
                     ? CodinGameApi.getPuzzleFromPrettyId(puzzle.getPrettyId())
                     : puzzle;
 
-    @Override
-    public void actualise() {
-        // TODO Auto-generated method stub
-    }
-
-    @Override
     public List<Puzzle> getPuzzles() {
         List<Integer> puzzlesId = CodinGameApi
                 .getAllMinifiedPuzzles()
