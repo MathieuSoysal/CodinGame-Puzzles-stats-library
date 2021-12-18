b75ee2f2a64eaef51dcd4f1af88e1a881600a7ea
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-18 11:07:18 +0100

    🧪 Add failing test : User

---
+++ src/test/java/com/github/mathieusoysal/codingame_stats/puzzle/UserTest.java
@@ -0,0 +1,50 @@
+package com.github.mathieusoysal.codingame_stats.puzzle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.google.gson.Gson;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class UserTest {
+
+    @Nested
+    class ConversionFromJsonTest {
+        final static String JSON_USER = """
+                {
+                    \"userId\": 155929,
+                    \"pseudo\": \"[CG]jupoulton\",
+                    \"publicHandle\": \"d39436e9a23b5060ed3efaf1c24b4ba8929551\",
+                    \"enable\": false,
+                    \"avatar\": 9399538004630,
+                    \"cover\": 10055855122642
+                }
+                """;
+
+        User user;
+
+        @BeforeEach
+        public void setUp() {
+            user = new Gson().fromJson(JSON_USER, User.class);
+        }
+
+        @Test
+        public void testGetUserId() {
+            assertEquals(155929, user.userId());
+        }
+
+        @Test
+        public void testGetPseudo() {
+            assertEquals("[CG]jupoulton", user.pseudo());
+        }
+
+        @Test
+        public void testGetPublicHandle() {
+            assertEquals("d39436e9a23b5060ed3efaf1c24b4ba8929551", user.publicHandle());
+        }
+
+    }
+
+}
