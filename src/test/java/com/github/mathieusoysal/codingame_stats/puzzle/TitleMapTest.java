b8eebd7c2755461e1cfdf21300ef14e9397f1f10
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-18 10:48:12 +0100

    🧪 Add failing test : TitleMap

---
+++ src/test/java/com/github/mathieusoysal/codingame_stats/puzzle/TitleMapTest.java
@@ -0,0 +1,41 @@
+package com.github.mathieusoysal.codingame_stats.puzzle;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.google.gson.Gson;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class TitleMapTest {
+
+    @Nested
+    class ConversionFromJsonTest {
+        final static String JSON_TITLEMAP = """
+                {
+                    \"1\": \"La Bataille\",
+                    \"2\": \"War\"
+                }
+                """;
+
+        TitleMap titleMap;
+
+        @BeforeEach
+        public void setUp() {
+            titleMap = new Gson().fromJson(JSON_TITLEMAP, TitleMap.class);
+        }
+
+        @Test
+        public void testGet1() {
+            assertEquals("La Bataille", titleMap._1());
+        }
+
+        @Test
+        public void testGet2() {
+            assertEquals("War", titleMap._2());
+        }
+
+    }
+
+}
