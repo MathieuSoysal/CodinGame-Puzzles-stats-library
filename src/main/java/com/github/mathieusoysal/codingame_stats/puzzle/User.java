c1205c471c573f88814e4238c3493525ebb09227
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-18 13:40:14 +0100

    🔨 Validation test : User

--- src/main/java/com/github/mathieusoysal/codingame_stats/puzzle/User.java
+++ src/main/java/com/github/mathieusoysal/codingame_stats/puzzle/User.java
@@ -6,6 +6,31 @@ public class User {
     private String publicHandle;
 
     public User(int userId, String pseudo, String publicHandle) {
+        this.userId = userId;
+        this.pseudo = pseudo;
+        this.publicHandle = publicHandle;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + userId;
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (userId != other.userId)
+            return false;
+        return true;
     }
 
     public int getUserId() {
