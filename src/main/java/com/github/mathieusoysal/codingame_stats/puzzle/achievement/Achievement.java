3ccc154cf0f57d4aa4efd3751e5cca01df8f3ab6
MathieuSoysal <Mathieu.Soysal@outlook.fr>
2021-12-18 12:50:25 +0100

    🔨 Validation test : Achievement

--- src/main/java/com/github/mathieusoysal/codingame_stats/puzzle/achievement/Achievement.java
+++ src/main/java/com/github/mathieusoysal/codingame_stats/puzzle/achievement/Achievement.java
@@ -17,8 +17,45 @@ public class Achievement {
         private double weight;
         private AchievementLevel level;
 
-        public Achievement(String string, int i, String string2, String string3, int j, int k, int l, int m, long n,
-                double d, AchievementLevel silver) {
+        Achievement(String id, int puzzleId, String title, String description, int points, int progress,
+                        int progressMax, int completionTime, long imageBinaryId, double weight,
+                        AchievementLevel level) {
+                this.id = id;
+                this.puzzleId = puzzleId;
+                this.title = title;
+                this.description = description;
+                this.points = points;
+                this.progress = progress;
+                this.progressMax = progressMax;
+                this.completionTime = completionTime;
+                this.imageBinaryId = imageBinaryId;
+                this.weight = weight;
+                this.level = level;
+        }
+
+        @Override
+        public int hashCode() {
+                final int prime = 31;
+                int result = 1;
+                result = prime * result + ((id == null) ? 0 : id.hashCode());
+                return result;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+                if (this == obj)
+                        return true;
+                if (obj == null)
+                        return false;
+                if (getClass() != obj.getClass())
+                        return false;
+                Achievement other = (Achievement) obj;
+                if (id == null) {
+                        if (other.id != null)
+                                return false;
+                } else if (!id.equals(other.id))
+                        return false;
+                return true;
         }
 
         public String getId() {
